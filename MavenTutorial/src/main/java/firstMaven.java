815a9868ba0315eff892251e2f5dbbb91c06be9c
n.kosev <n.kosev@IND-NKosev.indexbg.lirex.com>
2015-09-23 16:08:29 +0300

    PUSH COMMIT

--- MavenTutorial/src/main/java/firstMaven.java
+++ MavenTutorial/src/main/java/firstMaven.java
@@ -5,6 +5,8 @@ public class firstMaven {
 	public static void main(String[] args) {
 		System.out.println("Hello Niki!!!");
 		System.out.println("Hello Niki version2");
+		
+		System.out.println("Hello Niki PUSH version 2 ");
 	}
 
 }
